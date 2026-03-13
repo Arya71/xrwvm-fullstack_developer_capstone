1df0a9f0da240abe3f0e03d412644013924ccf3d
Arya71 <sawantarya203@gmail.com>
2026-03-13 20:25:15 +0530

    Capstone submission files

---
+++ README.md
@@ -0,0 +1,10 @@
+# fullstack_developer_capstone
+
+This is the final project for the IBM Cloud Application Development Capstone.
+
+Features:
+- Dealer listing
+- Dealer reviews
+- User authentication
+- Sentiment analysis
+- Review submission
---
+++ server/frontend/src/components/Register/Register.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Register() {
+  return (
+    <div>
+
+      <h2>Sign-up</h2>
+
+      <input placeholder="Username"/>
+      <input placeholder="First Name"/>
+      <input placeholder="Last Name"/>
+      <input placeholder="Email"/>
+      <input placeholder="Password"/>
+
+      <button>Register</button>
+
+    </div>
+  );
+}
+
+export default Register;
